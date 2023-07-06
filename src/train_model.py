bbc9a9f5907d68d8de95630d8df0aeb97622972b
youssef595 <youssef.benallal@emines.um6p.ma>
2023-07-06 14:31:44 +0100

    test--

--- src/train_model.py
+++ src/train_model.py
@@ -10,7 +10,7 @@ from omegaconf import DictConfig
 
 @hydra.main(config_path="../config", config_name="main", version_base=None)
 def train_model(config: DictConfig):
-    """Function to train the model"""
+    # """Function to train the model"""
 
     print(f"Train modeling using {config.data.processed}")
     print(f"Model used: {config.model.name}")
